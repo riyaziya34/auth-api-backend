d55d32f59575d4394a01a2ed1fdf3bc185f2dba7
Riyaziya BAno <riyaziyabano34@gmail.com>
2025-07-31 02:04:00 +0530

    fix: bug

--- server.js
+++ server.js
@@ -11,50 +11,51 @@ dotenv.config();
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", require("./authRouter.js"));
-const users = [];
+app.use(
+  cors({
+    origin: true, 
+    credentials: true, 
+  })
+);
 connectDB();
-app.use(cors({
-  origin: true,
-  credentials: true,
-}));
-app.options("*", cors());
 
-app.get("/users/:id", (req, res) => {
-  res.send(users);
-});
 
-app.post("/users/:id", async (req, res) => {
-  try {
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    console.log(salt);
-    console.log(hashedPassword);
-    const user = {
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword,
-    };
-    users.push(user);
-    res.status(201).json(user);
-  } catch {
-    res.status(500).send();
-  }
-});
-app.post("/users/Login/:id", async (req, res) => {
-  const user = users.find((user) => user.email == req.body.email);
-  if (user == null) {
-    return res.status(400).send("Cannot find user");
-  }
-  try {
-    if (await bcrypt.compare(req.body.password, user.password)) {
-      res.send("Login successful");
-    } else {
-      res.send("Password incorrect");
-    }
-  } catch {
-    res.status(500).send();
-  }
-});
+// app.get("/users/:id", (req, res) => {
+//   res.send(users);
+// });
+
+// app.post("/users/:id", async (req, res) => {
+//   try {
+//     const salt = await bcrypt.genSalt();
+//     const hashedPassword = await bcrypt.hash(req.body.password, salt);
+//     console.log(salt);
+//     console.log(hashedPassword);
+//     const user = {
+//       name: req.body.name,
+//       email: req.body.email,
+//       password: hashedPassword,
+//     };
+//     users.push(user);
+//     res.status(201).json(user);
+//   } catch {
+//     res.status(500).send();
+//   }
+// });
+// app.post("/users/Login/:id", async (req, res) => {
+//   const user = users.find((user) => user.email == req.body.email);
+//   if (user == null) {
+//     return res.status(400).send("Cannot find user");
+//   }
+//   try {
+//     if (await bcrypt.compare(req.body.password, user.password)) {
+//       res.send("Login successful");
+//     } else {
+//       res.send("Password incorrect");
+//     }
+//   } catch {
+//     res.status(500).send();
+//   }
+// });
 
 app.listen(5050, () => {
   console.log("Server running on port 5050");
